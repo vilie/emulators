4c1f0eb08493fd460e795444dc2a38dc8a3bef5e
Valentin Ilie <valentin.ilie@gmail.com>
2016-04-04 17:48:43 +0300

    chip8: Add OpenGL screen test
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

--- chip8/screen_opengl.c
+++ chip8/screen_opengl.c
@@ -1,25 +1,52 @@
 #include <GL/glut.h>
 
+#define HEIGHT 32
+#define WIDTH 64
+
+int matrix[WIDTH][HEIGHT];
+
+void refreshScreen() {
+	int i, j;
+	glClear(GL_COLOR_BUFFER_BIT);	
+	glPointSize(10.0f);
+	glBegin(GL_POINTS);
+	for(int i = 0; i < WIDTH; i++)
+		for(int j = 0; j < HEIGHT; j++)
+			if (matrix[i][j] == 1)
+				glVertex3f((float) (i - 32)/32.0, (float) (j - 16)/16.0, 0);
+	glEnd();
+	glFlush();
+}
+	
+
 void displayMe(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POLYGON);
-		glVertex3f(0.0, 0.0, 0.0);
-		glVertex3f(0.5, 0.0, 0.0);
-		glVertex3f(0.5, 0.5, 0.0);
-		glVertex3f(0.0, 0.5, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);	
+	glPointSize(10.0f);
+	glBegin(GL_POINTS);
+		glVertex3f(-1.0, -1.0, 0.0); // left, down
+		glVertex3f(1.0, 1.0, 0.0);   // right, up
+		glVertex3f(1.0, -1.0, 0.0);  // right, down
+		glVertex3f(-1.0, 1.0, 0.0);  // left, up
 	glEnd();
 	glFlush();
 }
 
+
+
 int main(int argc, char** argv)
 {
+	int i;
+
+	/* Test */
+	for(int i = 0; i < WIDTH; i+=2)
+		matrix[i][0] = 1;
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE);
-	glutInitWindowSize(300, 300);
+	glutInitWindowSize(640, 320);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Hello world :D");
-	glutDisplayFunc(displayMe);
+	glutCreateWindow("Emulator screen");
+	glutDisplayFunc(refreshScreen);
 	glutMainLoop();
 	return 0;
 }

3ddcb8108fe028f427f48914fc3fae12ef4700e8
Valentin Ilie <valentin.ilie@gmail.com>
2016-04-10 13:39:20 -0700

    8080: Jump tables stub
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

--- 8080/8080.c
+++ 8080/8080.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void doA() {
+	printf("Hello a\n");
+}
+
+void doB() {
+	printf("Hello b\n");
+}
+
+void doC() {
+	printf("Hello c\n");
+}
+
+void execInstruction(uint16_t opcode) {
+	int i = (opcode & 0xF000) >> 12;
+	void (*testCallback[3]) (void) = {doA, doB, doC};
+	testCallback[i]();
+}
 
 int main() {
-	printf("Hello World!");
+	uint16_t opcode = 0x1234;
+	execInstruction(opcode);
 	return 0;
 }
+
+

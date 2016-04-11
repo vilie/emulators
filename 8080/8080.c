23f0fadb99bbfdeb4b7097b6633e6a6b3ae171e8
Valentin Ilie <valentin.ilie@gmail.com>
2016-04-11 17:20:55 +0300

    8080: Add CPU struct
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

--- 8080/8080.c
+++ 8080/8080.c
@@ -3,14 +3,23 @@
 #include <stdint.h>
 #include "instructions.h"
 
+struct proc {
+	uint8_t A, flags;
+	uint8_t B, C;
+	uint8_t D, E;
+	uint8_t H, L;
+	uint16_t SP;
+	uint16_t PC;
+} pr;
+
 void execInstruction(uint16_t opcode) {
-	int i = (opcode & 0xF000) >> 12;
+	int i = (opcode & 0xF0) >> 4;
 	void (*testCallback[3]) (void) = {doA, doB, doC};
 	testCallback[i]();
 }
 
 int main() {
-	uint16_t opcode = 0x1234;
+	uint8_t opcode = 0x24;
 	execInstruction(opcode);
 	return 0;
 }

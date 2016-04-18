e9b5e09fd30518038b4546dd28933b154a736444
Valentin Ilie <valentin.ilie@gmail.com>
2016-04-18 16:52:22 +0300

    8080: Add ral/rar. Fix mask width issue
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

--- 8080/instructions.c
+++ 8080/instructions.c
@@ -42,15 +42,25 @@ void doC(uint8_t opcode) {}
 void doB(uint8_t opcode) {}
 
 void rcl(uint8_t opcode) {
-	pr.flags |= (pr.A & 0x1000) >> 7; /* CF set to most sign bit */
+	pr.flags |= (pr.A & 0x10) >> 7; /* CF set to most sign bit */
 	pr.A = pr.A << 1 | pr.A >> 7; /* rotate left 1 */
 }
 
 void rrc(uint8_t opcode) {
-	pr.flags |= (pr.A & 0x0001);
+	pr.flags |= (pr.A & 0x01);
 	pr.A = pr.A >> 1 | pr.A << 7; /* rotate right 1 */
 }
 
-void ral(uint8_t opcode) {}
+void ral(uint8_t opcode) {
+	uint8_t backupFlags = pr.flags; /* blackup CF */
+	rcl(opcode);
+	pr.flags = pr.flags & 0xFE + pr.A & 0x01; /* CF = least significant in A */
+	pr.A = pr.A & 0xFE + backupFlags & 0x01; /* A least significant = CF (backup) */
+}
 
-void rar(uint8_t opcode) {}
+void rar(uint8_t opcode) {
+	uint8_t backupFlags = pr.flags; /* backup CF */
+	rrc(opcode);
+	pr.flags = pr.flags & 0xFE + (pr.A >> 7); /* CF = most significant in A */
+	pr.A = pr.A & 0x7F + backupFlags << 7; /* A most significant = CF (backup) */
+}
